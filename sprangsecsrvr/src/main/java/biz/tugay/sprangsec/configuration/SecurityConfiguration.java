7bd45bc85e39068af155509c564001eebe7f512f
Koray Tugay <koraytugay@icloud.com>
2020-10-27 20:43:40 -0400

    Never create HttpSession.

--- sprangsecsrvr/src/main/java/biz/tugay/sprangsec/configuration/SecurityConfiguration.java
+++ sprangsecsrvr/src/main/java/biz/tugay/sprangsec/configuration/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
@@ -28,6 +29,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
         .authorizeRequests()
         // This is needed since Angular first sends an OPTIONS request to /validate endpoint
         // without any authorization information with the request.
