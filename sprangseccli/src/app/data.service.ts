333f1a74bac26c50b57a1b48004a382c894d07b4
Koray Tugay <koraytugay@icloud.com>
2020-10-27 21:22:08 -0400

    Block Basic Authentication popup in browser.

--- sprangseccli/src/app/data.service.ts
+++ sprangseccli/src/app/data.service.ts
@@ -12,7 +12,8 @@ export class DataService {
 
   validateUser(username: string, password: string): Observable<{jwtToken:string}> {
     const authData = btoa(`${username}:${password}`);  // binary to ascii - encodes input in base64
-    const headers = new HttpHeaders().append('Authorization', 'Basic ' + authData);
+    let headers = new HttpHeaders().append('Authorization', 'Basic ' + authData);
+    headers = headers.append('X-Requested-With', 'XMLHttpRequest');
     return this.http.get<{jwtToken:string}>('http://localhost:8080/validate',
       {headers: headers, withCredentials: true});
   }
